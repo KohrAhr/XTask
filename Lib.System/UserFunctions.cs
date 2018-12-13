e3fcfc7c4b53e3cb0f3183ce80df1647215b3f81
ZAM <ZAM@1CLICK.LV>
2018-12-13 06:25:22 +0200

    am i Admin? function added

--- Lib.System/UserFunctions.cs
+++ Lib.System/UserFunctions.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Security.Principal;
 
 namespace Lib.System
 {
@@ -135,5 +136,87 @@ namespace Lib.System
             return result;
         }
 
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <returns></returns>
+        //public static bool CurrentUserHasAdminRights()
+        //{
+        //    return new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+        //}
+
+        /// <summary>
+        /// Determines whether the specified user is an administrator.
+        /// </summary>
+        /// <param name="username">The user name.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified user is an administrator; otherwise, <c>false</c>.
+        /// </returns>
+        /// <seealso href="https://ayende.com/blog/158401/are-you-an-administrator"/>
+        public static bool IsCurrentUserAdmin(bool checkCurrentRole = true)
+        {
+            bool isElevated = false;
+
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                if (checkCurrentRole)
+                {
+                    // Even if the user is defined in the Admin group, UAC defines 2 roles: one user and one admin. 
+                    // IsInRole consider the current default role as user, thus will return false!
+                    // Will consider the admin role only if the app is explicitly run as admin!
+                    WindowsPrincipal principal = new WindowsPrincipal(identity);
+                    isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+                }
+                else
+                {
+                    // read all roles for the current identity name, asking ActiveDirectory
+                    isElevated = IsAdministratorNoCache(identity.Name);
+                }
+            }
+
+            return isElevated;
+        }
+
+        /// <summary>
+        /// Determines whether the specified user is an administrator.
+        /// </summary>
+        /// <param name="username">The user name.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified user is an administrator; otherwise, <c>false</c>.
+        /// </returns>
+        /// <seealso href="https://ayende.com/blog/158401/are-you-an-administrator"/>
+        private static bool IsAdministratorNoCache(string username)
+        {
+            PrincipalContext ctx;
+            try
+            {
+                Domain.GetComputerDomain();
+                try
+                {
+                    ctx = new PrincipalContext(ContextType.Domain);
+                }
+                catch (PrincipalServerDownException)
+                {
+                    // can't access domain, check local machine instead 
+                    ctx = new PrincipalContext(ContextType.Machine);
+                }
+            }
+            catch (ActiveDirectoryObjectNotFoundException)
+            {
+                // not in a domain
+                ctx = new PrincipalContext(ContextType.Machine);
+            }
+            var up = UserPrincipal.FindByIdentity(ctx, username);
+            if (up != null)
+            {
+                PrincipalSearchResult<Principal> authGroups = up.GetAuthorizationGroups();
+                return authGroups.Any(principal =>
+                                      principal.Sid.IsWellKnown(WellKnownSidType.BuiltinAdministratorsSid) ||
+                                      principal.Sid.IsWellKnown(WellKnownSidType.AccountDomainAdminsSid) ||
+                                      principal.Sid.IsWellKnown(WellKnownSidType.AccountAdministratorSid) ||
+                                      principal.Sid.IsWellKnown(WellKnownSidType.AccountEnterpriseAdminsSid));
+            }
+            return false;
+        }
     }
 }
